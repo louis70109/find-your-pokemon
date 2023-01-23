e99016384a35b0a7cf4f2b82b29155ebe12cc58d
Nijia Lin <louis70109@gmail.com>
2023-01-24 01:36:08 +0800

    ⚡ Add develop env controller

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ if os.getenv('API_ENV') != 'production':
 
 import uvicorn
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 
 from routers import webhooks
 
@@ -25,10 +24,8 @@ async def root():
     return {"message": "Hello World!"}
 
 
-@app.get("/liff", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    port = os.environ.get('PORT', default=8080)
+    debug = True if os.environ.get('API_ENV', default='develop') == 'develop' else False
+    print('==========start===========')
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=debug)
