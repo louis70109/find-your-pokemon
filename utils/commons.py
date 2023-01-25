21e23d5d19796724bf2847ba76e4d39c9c23f2dd
Nijia Lin <louis70109@gmail.com>
2023-01-25 22:05:57 +0800

    🔥 remove commons.py

--- utils/commons.py
+++
@@ -1,9 +0,0 @@
-import requests
-def find_CN_by_EN_name(request_json, name='Fire Punch') -> list:
-  return_data = []
-  data = request_json['data']
-  for da in data:
-    if da['nameEn'] == name:
-      return_data = da
-      break
-  return return_data
