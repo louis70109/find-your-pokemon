09219205c31928a9c02ce3a7f329992e912dfbac
Nijia Lin <louis70109@gmail.com>
2023-01-25 18:54:33 +0800

    ♻️  add condition to ignore non-series pokemon

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -52,49 +52,65 @@ def message_text(event):
 
     if re.findall("^find\s+.*", message):
         message = message.split(' ')[1].lower()
+        
         series = os.getenv('SERIES')
         url = "https://www.pikalytics.com/pokedex/"+series+'/'+message
+        logger.info('Pokemon series web crawler query: ' + url)
+
         rs = requests.get(url=url)
         specific_soup = BeautifulSoup(rs.text, "html.parser")
-        logger.info('=====技能=====')
-        skills = specific_soup.select('.pokedex-category-wrapper')
-        contents = []
-        logger.info(message+' 相關戰績')
-        # 或許可以把對應 API 都放在 mapping 裏面
-        pikalytic_mapping = {1: '招式', 2: '隊友', 3: '物品', 4: '特性', 5: '努力值'}
-        for index in range(1, len(skills)-1):
-            s = skills[index].select('.pokedex-move-entry-new')
-            item, items = '', []
-            logger.info(f'搜尋 {pikalytic_mapping[index]}')
-            # 整理爬蟲下來各個清單對應的陣列
-            for entry_idx in range(0, len(s)-1):
-                item = arrange_text(s[entry_idx].text)
-                items.append(item)
-            if index == 1:
-                with sqlite.connect() as con:
-                    for it in items:
-                        item_query = sqlite.exec_one(con, 
-                        f"SELECT name_zh, category FROM t_move WHERE name_en == '{it[0]}'")
-                        it[0] = item_query[0]
-                        it[1] = item_query[1]
-                logger.info('Move query result: '+ str(items))
-            if index == 3:
-                # 針對物品欄把 request 拉出來寫在放入函式
-                # 若之後重複可單獨在包裝
-                with sqlite.connect() as con:
-                    for it in items:
-                        item_query = sqlite.exec_one(con, 
-                        f"SELECT name_zh, img_url FROM t_item WHERE name_en == '{it[0]}'")
-                        logger.info('Item query: '+str(item_query))
-                        it[0] = item_query[0]
-                logger.info('Item query result: '+ str(items))
-            # 輸出成 Flex，若 API 有圖片可能需要另外處理
-            contents.append(skill_list(
-                name=pikalytic_mapping[index], abilities=items, url=url))
-        response = FlexSendMessage(alt_text=message, contents={
-            "type": "carousel",
-            "contents": contents
-        })
+        details = specific_soup.select('.pokedex-category-wrapper')
+        logger.info(message+' record start...')
+        
+        if len(details) == 0:
+            logger.info(message+' is not in this series.')
+            response = TextSendMessage('找不到 '+message)
+        else:
+            contents = []
+            # 或許可以把對應 API 都放在 mapping 裏面
+            pikalytic_mapping = {1: '招式', 2: '隊友', 3: '物品', 4: '特性', 5: '努力值'}
+            for index in range(1, len(details)):
+                s = details[index].select('.pokedex-move-entry-new')
+                item, items = '', []
+                logger.info(f'搜尋 {pikalytic_mapping[index]}')
+                # 整理爬蟲下來各個清單對應的陣列
+                for entry_idx in range(0, len(s)-1):
+                    if index == 5 and entry_idx == 5:
+                        break
+                    item = arrange_text(s[entry_idx].text)
+                    items.append(item)
+
+                if index == 1:
+                    with sqlite.connect() as con:
+                        for it in items:
+                            item_query = sqlite.exec_one(con, 
+                            f"SELECT name_zh, category FROM t_move WHERE name_en == '{it[0]}'")
+                            it[0] = item_query[0]
+                            it[1] = item_query[1]
+                    logger.info('Move query result: '+ str(items))
+                elif index == 3:
+                    # 針對物品欄把 request 拉出來寫在放入函式
+                    # 若之後重複可單獨在包裝
+                    with sqlite.connect() as con:
+                        for it in items:
+                            item_query = sqlite.exec_one(con, 
+                            f"SELECT name_zh, img_url FROM t_item WHERE name_en == '{it[0]}'")
+                            it[0] = item_query[0]
+                    logger.info('Item query result: '+ str(items))
+                elif index == 5: # 努力值, 長度==8
+                    with sqlite.connect() as con:
+                        for it in items:
+                            item_query = sqlite.exec_one(con, 
+                            f"SELECT name_zh FROM t_nature WHERE name_en == '{it[0]}'")
+                            it[0] = item_query[0]
+                    logger.info('Nature query result: '+ str(items))
+                # 輸出成 Flex，若 API 有圖片可能需要另外處理
+                contents.append(skill_list(
+                    name=pikalytic_mapping[index], abilities=items, url=url))
+            response = FlexSendMessage(alt_text=message, contents={
+                "type": "carousel",
+                "contents": contents
+            })
     else:
         pokemon_row_list = pokemon_wiki(message)
 
