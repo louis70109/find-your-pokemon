7cfcf975367de9406b13370dc50bc8cee22908f2
Nijia Lin <louis70109@gmail.com>
2023-02-03 01:05:43 +0800

    ✨ add TOP and healthz check reply message

--- rank_crawler.py
+++ rank_crawler.py
@@ -15,51 +15,50 @@ all_trending = soup.select('.float-right.margin-right-20')
 # print(all_trending[0].text.rstrip())
 for i in range(len(all_rank)):
     print(all_rank[i].text.rstrip()+" "+all_trending[i].text.rstrip())
-    print()
 
-print('-------Find one--------')
-print('可放入特定的名字')
-series = 'gen9vgc2023series1'
-
-
-def arrange_text(text: str):
-    return list(filter(None, text.rstrip().split('\n')))
+# print('-------Find one--------')
+# print('可放入特定的名字')
+# series = 'gen9vgc2023series1'
 
 
-rs = requests.get(url=url+series+'/meowscarada')
-specific_soup = BeautifulSoup(rs.text, "html.parser")
-print(specific_soup.select('.pokedex-category-wrapper'))
-print('=====技能=====')
-skills = specific_soup.select('.pokedex-category-wrapper')[1]  # skills
-skill = skills.select('.pokedex-move-entry-new')[0].text
-skill_text = arrange_text(skill)
-
-print(skill_text)
+# def arrange_text(text: str):
+#     return list(filter(None, text.rstrip().split('\n')))
 
 
-# print('-------隊友-------')
-# teammates = specific_soup.select('.pokedex-category-wrapper')[2]  # teammates
-# teammate = teammates.select('.pokedex-move-entry-new')[0].text
-# print(arrange_text(teammate))
+# rs = requests.get(url=url+series+'/meowscarada')
+# specific_soup = BeautifulSoup(rs.text, "html.parser")
+# print(specific_soup.select('.pokedex-category-wrapper'))
+# print('=====技能=====')
+# skills = specific_soup.select('.pokedex-category-wrapper')[1]  # skills
+# skill = skills.select('.pokedex-move-entry-new')[0].text
+# skill_text = arrange_text(skill)
 
-# print('-------物品-------')
-# items = specific_soup.select('.pokedex-category-wrapper')[3]
-# item = items.select('.pokedex-move-entry-new')[0].text
-# print(arrange_text(item))
-
-# print('-------能力-------')
-# abilities = specific_soup.select('.pokedex-category-wrapper')[4]
-# ability = abilities.select('.pokedex-move-entry-new')[0].text
-# print(arrange_text(ability))
-
-# print('-------努力值-------')
-# evs = specific_soup.select('.pokedex-category-wrapper')[5]
-# ev = evs.select('.pokedex-move-entry-new')[0].text
-# print(arrange_text(ev))
+# print(skill_text)
 
 
-from utils import sqlite
-with sqlite.connect() as con:
-        query = sqlite.exec(con, 
-        f"SELECT name_zh FROM t_item WHERE name_en == 'Growth Mulch'")
-        print(query[0][0])
+# # print('-------隊友-------')
+# # teammates = specific_soup.select('.pokedex-category-wrapper')[2]  # teammates
+# # teammate = teammates.select('.pokedex-move-entry-new')[0].text
+# # print(arrange_text(teammate))
+
+# # print('-------物品-------')
+# # items = specific_soup.select('.pokedex-category-wrapper')[3]
+# # item = items.select('.pokedex-move-entry-new')[0].text
+# # print(arrange_text(item))
+
+# # print('-------能力-------')
+# # abilities = specific_soup.select('.pokedex-category-wrapper')[4]
+# # ability = abilities.select('.pokedex-move-entry-new')[0].text
+# # print(arrange_text(ability))
+
+# # print('-------努力值-------')
+# # evs = specific_soup.select('.pokedex-category-wrapper')[5]
+# # ev = evs.select('.pokedex-move-entry-new')[0].text
+# # print(arrange_text(ev))
+
+
+# from utils import sqlite
+# with sqlite.connect() as con:
+#         query = sqlite.exec(con, 
+#         f"SELECT name_zh FROM t_item WHERE name_en == 'Growth Mulch'")
+#         print(query[0][0])
--- routers/webhooks.py
+++ routers/webhooks.py
@@ -2,12 +2,16 @@ import logging
 import os
 from typing import List, Optional
 import re
+import requests
 from controller.find_pokemon import find_specific_pokemon_all_status, search_specific_pokemon_by_wiki
 from fastapi import APIRouter, HTTPException, Header, Request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import TextMessage, MessageEvent, ImageSendMessage
+from linebot.models import TextMessage, MessageEvent, ImageSendMessage, TextSendMessage
 from pydantic import BaseModel
+from bs4 import BeautifulSoup
+
+from utils.poke_crawler import find_pokemon_name
 
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
@@ -45,6 +49,23 @@ def message_text(event):
     if message == '屬性':
         image_url = 'https://raw.githubusercontent.com/louis70109/find-your-pokemon/main/pokemon.jpg'
         response = ImageSendMessage(image_url, image_url)
+    elif message == 'TOP':
+        url = "https://www.pikalytics.com/pokedex/"
+        r = requests.get(url=url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        all_rank = soup.select('span.pokemon-name')
+        all_trending = soup.select('.float-right.margin-right-20')
+
+        trending = ''
+        for i in range(len(all_rank)):
+            en_name = all_rank[i].text.rstrip()
+            zh_name, _ = find_pokemon_name(en_name)
+            trend_percent = all_trending[i].text.rstrip()
+            trending += f'{zh_name} {trend_percent}'
+        response = TextSendMessage(trending)
+    elif message == 'Heal':
+        res = requests.get(os.getenv('HEAL_URL'))
+        response = TextSendMessage(str(res.json()))
     elif re.findall("^find\s+.*", message):
         message = message.split(' ')[1].lower()
         response = find_specific_pokemon_all_status(pokemon_name=message)
