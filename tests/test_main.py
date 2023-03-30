59669385b5dacbada01b4e384a374698617d2d04
Nijia Lin <louis70109@gmail.com>
2023-03-30 15:07:54 +0800

    ♻️  fix config and healthz check

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,7 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/healthz")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World!"}
 
